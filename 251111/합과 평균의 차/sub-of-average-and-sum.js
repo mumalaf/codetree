5535def8b4749cbf3dbbc6fbe07146d8a061211e
Dayeon <dayeon99j@gmail.com>
2025-11-11 16:06:19 +0900

    Title: 합과 평균의 차, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251111/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(' ');
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+let sum = a+b+c;
+let avg = sum/3;
+
+console.log(sum);
+console.log(avg);
+console.log(sum-avg);
