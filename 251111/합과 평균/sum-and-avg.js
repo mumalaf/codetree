617641d6e6ae38a110a9a490db2ab6dcc39b2e03
Dayeon <dayeon99j@gmail.com>
2025-11-11 16:04:17 +0900

    Title: 합과 평균, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 251111/합과 평균/sum-and-avg.js
+++ 251111/합과 평균/sum-and-avg.js
@@ -3,6 +3,6 @@ let input = fs.readFileSync(0).toString().split(' ');
 let a = Number(input[0]);
 let b = Number(input[1]);
 let sum = a+b;
-let avg = sum/2;
+let avg = (sum/2).toFixed(1);
 
 console.log(sum + ' ' + avg);
