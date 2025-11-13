bbd322fd4d3cbb3a97262289fb72896f8779a42d
Dayeon <dayeon99j@gmail.com>
2025-11-13 17:56:44 +0900

    Title: 윤년인가, Time: 60ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251113/윤년인가/is-leap-year.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let year = Number(fs.readFileSync(0).toString().trim());
+
+if (year%4==0){
+    if((year%100==0)&&(year%400!=0)){
+        console.log('false');
+    }else{
+        console.log('true');
+    }
+}
