eea0f94d5397fb97de89e115cead1ec2b3670cbb
Dayeon <dayeon99j@gmail.com>
2025-11-20 16:51:29 +0900

    Title: 더 긴 문자열, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 251120/더 긴 문자열/longer-string.js
+++ 251120/더 긴 문자열/longer-string.js
@@ -3,6 +3,8 @@ const input = fs.readFileSync(0).toString().trim().split(' ');
 
 if(input[0].length>input[1].length){
     console.log(input[0],input[0].length);
+}else if(input[0].length==input[1].length){
+    console.log("same");
 }else{
     console.log(input[1],input[1].length);
 }
