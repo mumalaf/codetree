d98475d799e71f935bfa71266faea02f268fd658
Dayeon <dayeon99j@gmail.com>
2025-11-22 16:54:31 +0900

    Title: 특정 수 맞히기, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251122/특정 수 맞히기/catching-specific-number.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+for (let i=0; i<input.length; i++){
+    if(input[i]<25){
+        console.log("Higher");
+    }else if(input[i]>25){
+        console.log("Lower");
+    }else{
+        console.log("Good");
+    }
+}
